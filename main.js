64a3d1b7ee5a96ed96215c19a734c730967bd97e
Marco Aurelio Hansen <m.hansendeoliveira@gmail.com>
2021-10-24 14:44:20 -0300

    Projeto NLW Heat
    Subi com o projeto pronto

---
+++ main.js
@@ -0,0 +1,33 @@
+const socialMediaLinks = {
+  github: 'marcoahansen',
+  youtube: 'MarcoAurelioHansen',
+  facebook: 'marcoaureliohansen',
+  instagram: 'marcoahansen',
+  twitter: 'marcoahansen'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${socialMediaLinks[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
